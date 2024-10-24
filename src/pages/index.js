103b3e33c881167bbcb19399ccf1394045b90f3e
Rahul Prajapati <prajapatirahul9110@gmail.com>
2024-10-24 19:13:49 +0530

    url updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,8 @@ const Home = () => {
 
   useEffect(() => {
     // Initialize WebSocket connection
-    // const socket = new WebSocket("ws://localhost:3000/ws");
-    const socket = new WebSocket(process.env.NEXT_PUBLIC_WEB_SOCKET);
+    const socket = new WebSocket("ws://localhost:3000/ws");
+    // const socket = new WebSocket(process.env.NEXT_PUBLIC_WEB_SOCKET);
 
     socket.onopen = () => {
       console.log("WebSocket connected");
