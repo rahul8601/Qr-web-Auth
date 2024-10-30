b8dee32749bf7a40b2fe01ec3eedb8b7c0037fd1
Rahul Prajapati <prajapatirahul9110@gmail.com>
2024-10-30 11:37:37 +0530

    ui updated

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -14,7 +14,7 @@ const UserDetails = ({ authenticatedUser, data }) => {
         Authenticated as User: {authenticatedUser}
       </h2>
       {/* <button
-            onClick={() => console.log("click Logout")}
+            onClick={() => console.log("click Logout")}s
             className="mt-4 py-2 px-4 bg-red-500 text-white rounded-md hover:bg-red-600 transition duration-300"
           >
             Logout
